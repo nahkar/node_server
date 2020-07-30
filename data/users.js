ba2c5615c327b06b0eccaeb2eedaf48c56fd4a09
Belenov Dmitriy <dimabelenov@gmail.com>
2020-07-30 21:14:10 +0300

    adds some users fake data

--- data/users.js
+++ data/users.js
@@ -3,6 +3,9 @@ const users = [
 	{ id: 2, name: 'Vova', age: 54 },
 	{ id: 3, name: 'Petya', age: 44 },
 	{ id: 4, name: 'Valentin', age: 33 },
+	{ id: 5, name: 'Valera', age: 27 },
+	{ id: 6, name: 'Nataha', age: 20 },
+	{ id: 7, name: 'Irina', age: 18 },
 ];
 
 module.exports = users;
