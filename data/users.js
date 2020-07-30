0abe1985c9031dfa5e0bfcbafe7ec38f80090dbd
Nikita Voskoboinikov <nikitavoskoboinikov2828@gmail.com>
2020-07-30 21:32:12 +0300

    First change data

--- data/users.js
+++ data/users.js
@@ -7,7 +7,6 @@ const users = [
 	{ id: 6, name: 'Nataha', age: 20 },
 	{ id: 7, name: 'Irina', age: 18 },
 	{ id: 8, name: 'Petr', age: 23 },
-	{ id: 9, name: 'Edik', age: 15},
 ];
 
 module.exports = users;
